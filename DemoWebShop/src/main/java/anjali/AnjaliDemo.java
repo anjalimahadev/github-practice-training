d84c619c9b7d9cb4b88b9f7ebd5efdbe4c6d79c3
jsoftchannel <mahadevkjo@.com>
2025-07-01 13:17:12 +0530

    AnjaliDemo

---
+++ DemoWebShop/src/main/java/anjali/AnjaliDemo.java
@@ -0,0 +1,12 @@
+package anjali;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AnjaliDemo {
+	@Test 
+	public void demo() {
+		WebDriver driver=new ChromeDriver();
+	}
+}
